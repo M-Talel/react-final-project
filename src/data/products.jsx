89f1884f0db5164da2ddc3ef06aaa942f1752223
jst.talel <mathewstalel@outlook.com>
2026-05-17 16:03:45 +0300

    Commit to include uncommited files

---
+++ src/data/products.jsx
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Oversized Hoodie",
+    price: 45,
+    category: "Hoodies",
+    stock: 12,
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+  },
+  {
+    id: 2,
+    name: "Slim Fit Jeans",
+    price: 60,
+    category: "Jeans",
+    stock: 8,
+    image:
+      "https://images.unsplash.com/photo-1541099649105-f69ad21f3246"
+  },
+  {
+    id: 3,
+    name: "Graphic T-Shirt",
+    price: 25,
+    category: "T-Shirts",
+    stock: 20,
+    image:
+      "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c"
+  }
+];
+
+export default products;
