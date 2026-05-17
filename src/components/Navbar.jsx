a087258a47525a3e3149804929916efb6b8f071c
jst.talel <mathewstalel@outlook.com>
2026-05-17 16:01:36 +0300

    Included the navbar to the website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>UrbanWear Admin</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Add Product</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
