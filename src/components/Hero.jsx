08108d4766e3f16da72385559e3c7e0207b5d644
jst.talel <mathewstalel@outlook.com>
2026-05-17 16:03:02 +0300

    Changes to hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h2>Clothing Store Admin Dashboard</h2>
+
+      <p>
+        Manage products, inventory, and clothing collections
+        easily.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
